82b268d06198e2927f223225e2455ef575b99177
Vladimir Sokolovsky <vlad@nvidia.com>
2026-03-30 19:58:18 -0500

    OobUpdate.py: Added a workaround for BMC tasks running in parallel
    Issue: 4948318
    Signed-off-by: Vladimir Sokolovsky <vlad@nvidia.com>

--- OobUpdate.py
+++ OobUpdate.py
@@ -85,6 +85,15 @@ def create_cfg_file(username, password, ssh_username, ssh_password, task_dir, ta
             else:
                 cfg_file.write('UPLOAD_CONFIG_IMAGE="no"\n')
 
+            # Check all the files under workaround directory and append their content to the configuration file
+            workaround_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'workaround')
+            if os.path.exists(workaround_dir):
+                for file in os.listdir(workaround_dir):
+                    if file.endswith('.cfg'):
+                        with open(os.path.join(workaround_dir, file), 'r') as workaround_file:
+                            cfg_file.write(workaround_file.read() + "\n")
+                        print("Added workaround file: {}".format(os.path.join(workaround_dir, file)))
+
             if bfcfg:
                 try:
                     with open(bfcfg, 'r') as bfcfg_file:
